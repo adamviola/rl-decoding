306b3e20bfb69409261cd04b60091af162435871
Adam Viola <aviola@umass.edu>
2021-12-04 03:01:55 -0500

    Added GPU support for generate and adjusted batch size to fix memory issues

--- generate.py
+++ generate.py
@@ -5,14 +5,14 @@ import math
 from tqdm import tqdm
 
 model_name = 'Helsinki-NLP/opus-mt-en-fr'
-batch_size = 256
-
+batch_size = 32
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Sample translations from English sentences using the given model
 # Returns the rewards (log-probs) for each sentence using pre-trained MarianMTModel
 def generate_data(sentences, translations_per_sentence, model=None):
     tokenizer = MarianTokenizer.from_pretrained(model_name)
-    evaluator = MarianMTModel.from_pretrained(model_name)
+    evaluator = MarianMTModel.from_pretrained(model_name).to(device)
 
     if model is None:
         model = evaluator
@@ -33,12 +33,12 @@ def generate_data(sentences, translations_per_sentence, model=None):
 
         # Tokenize batch of sentences
         encoder_input = tokenizer(batch, return_tensors='pt', padding=True)
-        input_ids = encoder_input['input_ids']
-        attention_mask = encoder_input['attention_mask']
+        input_ids = encoder_input['input_ids'].to(device)
+        attention_mask = encoder_input['attention_mask'].to(device)
 
         input_lengths = attention_mask.sum(dim=1)
         for input_id, input_length in zip(input_ids, input_lengths):
-            tokenized_sentences.append(input_id[:input_length])
+            tokenized_sentences.append(input_id[:input_length].cpu())
 
         # Generate translations
         results = model.generate(
@@ -57,12 +57,12 @@ def generate_data(sentences, translations_per_sentence, model=None):
 
         # Compute unpadded tokenized translations
         decoder_input_ids = results.sequences
-        decoder_attention_mask = torch.zeros_like(decoder_input_ids)
+        decoder_attention_mask = torch.zeros_like(decoder_input_ids).to(device)
         for i, sequence in enumerate(decoder_input_ids):
             pad_indices = (sequence[1:] == pad_token_id).nonzero()
             seq_len = len(sequence) - 1 if len(pad_indices) == 0 else pad_indices[0].item()
 
-            tokenized_translations.append(sequence[:seq_len + 1])
+            tokenized_translations.append(sequence[:seq_len + 1].cpu())
             decoder_attention_mask[i, :seq_len] = 1
 
         # Compute reward using evaluator model (log probs)
@@ -77,12 +77,12 @@ def generate_data(sentences, translations_per_sentence, model=None):
             seq_len = torch.sum(decoder_attention_mask[i])
             seq_log_probs = log_probs[i, :seq_len]
             seq_rewards = seq_log_probs[range(seq_len), sequence[1:seq_len + 1]]
-            rewards.append(seq_rewards)
+            rewards.append(seq_rewards.cpu())
 
     return list(zip(tokenized_sentences, tokenized_translations, rewards))
 
-# sentences = ['Hello!', 'How are you?',  'I am fine.', 'I am fine.', 'Thank you Thank you Thank you Thank you Thank you Thank you Thank you']
 
-# data = generate_data(sentences, 1)
-
-# print(data[0])
+if __name__ == '__main__':
+    sentences = ['Hello!', 'How are you?',  'I am fine.', 'I am fine.', 'Thank you Thank you Thank you Thank you Thank you Thank you Thank you']
+    data = generate_data(sentences, 1)
+    print(data[0])

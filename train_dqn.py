8d167b0f8c7dbd963f8e0c7cd01bdd523d6019c3
Adam Viola <aviola@umass.edu>
2021-12-05 20:18:46 -0500

    Added code to main.ipynb to train models

--- train_dqn.py
+++ train_dqn.py
@@ -4,7 +4,6 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TestTubeLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from generate import generate_data
 
 def load_data(paths):
     data = []
@@ -14,45 +13,48 @@ def load_data(paths):
         data.extend(section)
     return data
 
-if not torch.cuda.is_available():
-    print("\n\nWARNING: No GPU found\n\n")
 
-batch_size = 32
+def train_dqn(checkpoint_path=None):
+    if not torch.cuda.is_available():
+        print("\n\nWARNING: No GPU found\n\n")
 
-checkpoint_path = None
+    batch_size = 32
 
-# Read in data
-train_data_paths = [
-    'train_data/news-test200832.pt',
-    'drive/MyDrive/DQL_Data/newstest200932.pt',
-    'drive/MyDrive/DQL_Data/newstest201032.pt',
-    'drive/MyDrive/DQL_Data/newstest201132.pt',
-    'drive/MyDrive/DQL_Data/newstest201232.pt',
-    'drive/MyDrive/DQL_Data/newstest2014-fren32.pt',
-]
-train_data = load_data(train_data_paths)
-val_data = load_data(['train_data/newsdiscussdev2015-enfr32.pt'])
+    # Read in data
+    train_data_paths = [
+        'train_data/news-test200832.pt',
+        'drive/MyDrive/DQL_Data/newstest200932.pt',
+        'drive/MyDrive/DQL_Data/newstest201032.pt',
+        'drive/MyDrive/DQL_Data/newstest201132.pt',
+        'drive/MyDrive/DQL_Data/newstest201232.pt',
+        'drive/MyDrive/DQL_Data/newstest2014-fren32.pt',
+    ]
+    train_data = load_data(train_data_paths)
+    val_data = load_data(['train_data/newsdiscussdev2015-enfr32.pt'])
 
-# Create dataloaders for data
-train_dataloader = DataLoader(train_data, batch_size, shuffle=True, collate_fn=DeepQLearning.collate_fn)
-val_dataloader = DataLoader(val_data, batch_size, collate_fn=DeepQLearning.collate_fn)
+    # Create dataloaders for data
+    train_dataloader = DataLoader(train_data, batch_size, shuffle=True, collate_fn=DeepQLearning.collate_fn)
+    val_dataloader = DataLoader(val_data, batch_size, collate_fn=DeepQLearning.collate_fn)
 
-learner = DeepQLearning()
+    learner = DeepQLearning()
 
-callbacks = [
-    ModelCheckpoint(
-        monitor='val_loss',
-        filename='{epoch}-{val_loss:.3f}'
-    ),
-    EarlyStopping(monitor='val_loss', patience=5)
-]
+    callbacks = [
+        ModelCheckpoint(
+            monitor='val_loss',
+            filename='{epoch}-{val_loss:.3f}'
+        ),
+        EarlyStopping(monitor='val_loss', patience=5)
+    ]
 
-# Train model
-logger = TestTubeLogger("drive/MyDrive/DQL_Data", name="dqn")
-trainer = pl.Trainer(
-    callbacks=callbacks,
-    logger=logger,
-    accumulate_grad_batches=2,
-    gpus=1 if torch.cuda.is_available() else 0
-)
-trainer.fit(learner, train_dataloader, val_dataloader, ckpt_path=checkpoint_path)
+    # Train model
+    logger = TestTubeLogger("drive/MyDrive/DQL_Data", name="dqn")
+    trainer = pl.Trainer(
+        callbacks=callbacks,
+        logger=logger,
+        accumulate_grad_batches=2,
+        gpus=1 if torch.cuda.is_available() else 0
+    )
+    trainer.fit(learner, train_dataloader, val_dataloader, ckpt_path=checkpoint_path)
+
+if __name__ == "__main__":
+    train_dqn()
--- train_reinforce.py
+++ train_reinforce.py
@@ -12,42 +12,45 @@ def load_sentences(paths):
         sentences += readSGM(path)
     return sentences
 
-if not torch.cuda.is_available():
-    print("\n\nWARNING: No GPU found\n\n")
+def train_reinforce(checkpoint_path=None):
 
-batch_size = 32
-epoch_size = 256 # Size of generated training data/epoch
-dataloader_interval = 2 # How many times to re-use generated training data/epoch
-val_interval = 64 # Number of epochs before checking validation
+    if not torch.cuda.is_available():
+        print("\n\nWARNING: No GPU found\n\n")
 
-checkpoint_path = None
+    batch_size = 32
+    epoch_size = 256 # Size of generated training data/epoch
+    dataloader_interval = 2 # How many times to re-use generated training data/epoch
+    val_interval = 64 # Number of epochs before checking validation
 
-# Read English sentences from file; replace this later
-train_data = load_sentences([pair[0] for pair in TRAIN_PAIRS])
-val_data = load_sentences([pair[0] for pair in VAL_PAIRS])
+    # Read English sentences from file; replace this later
+    train_data = load_sentences([pair[0] for pair in TRAIN_PAIRS])
+    val_data = load_sentences([pair[0] for pair in VAL_PAIRS])
 
-# Create model
-learner = ReinforceBaseline(train_data, val_data, batch_size=batch_size, epoch_size=epoch_size)
+    # Create model
+    learner = ReinforceBaseline(train_data, val_data, batch_size=batch_size, epoch_size=epoch_size)
 
-callbacks = [
-    ModelCheckpoint(
-        monitor='val_mean_log_prob',
-        filename='{epoch}-{val_mean_log_prob:.3f}',
-        mode='max',
-    ),
-    EarlyStopping(monitor='val_mean_log_prob', patience=5, mode='max')
-]
+    callbacks = [
+        ModelCheckpoint(
+            monitor='val_mean_log_prob',
+            filename='{epoch}-{val_mean_log_prob:.3f}',
+            mode='max',
+        ),
+        EarlyStopping(monitor='val_mean_log_prob', patience=5, mode='max')
+    ]
 
-# Train model
-logger = TestTubeLogger("drive/MyDrive/DQL_Data", name="reinforce")
-trainer = pl.Trainer(
-    callbacks=callbacks,
-    logger=logger,
-    reload_dataloaders_every_n_epochs=dataloader_interval,
-    num_sanity_val_steps=0,
-    check_val_every_n_epoch=val_interval,
-    log_every_n_steps=4,
-    accumulate_grad_batches=2,
-    gpus=1 if torch.cuda.is_available() else 0
-)
-trainer.fit(learner, ckpt_path=checkpoint_path)
+    # Train model
+    logger = TestTubeLogger("drive/MyDrive/DQL_Data", name="reinforce")
+    trainer = pl.Trainer(
+        callbacks=callbacks,
+        logger=logger,
+        reload_dataloaders_every_n_epochs=dataloader_interval,
+        num_sanity_val_steps=0,
+        check_val_every_n_epoch=val_interval,
+        log_every_n_steps=4,
+        accumulate_grad_batches=2,
+        gpus=1 if torch.cuda.is_available() else 0
+    )
+    trainer.fit(learner, ckpt_path=checkpoint_path)
+
+if __name__ == "__main__":
+    train_reinforce()

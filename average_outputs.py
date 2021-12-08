e73f124657868ff680195b38ca6db14a281675b5
Jerry Fu <jerry.fu98@gmail.com>
2021-12-07 20:11:51 -0500

    added corpus bleu

--- average_outputs.py
+++ average_outputs.py
@@ -1,6 +1,7 @@
 from constants import OUTPUTS_PREFIX
 import pandas as pd
 import numpy as np
+from nltk.translate.bleu_score import corpus_bleu
 
 METRIC_COLUMNS = ['BLEU Score', '1-Gram', '2-Gram', '3-Gram', '4-Gram']
 
@@ -9,6 +10,13 @@ def average_BLEU_data(filename):
     print(filename)
     for cname in METRIC_COLUMNS:
         print(cname + ": " + str(np.mean(df[cname].tolist())))
+    # Corpus Bleu score
+    refs = [[sentance.split()] for sentance in df["Original French"]]
+    hyps = [sentance.split() for sentance in df["Generated French"]]
+    corp_bleu = corpus_bleu(refs, hyps)
+    print("Corpus Bleu" + ": " + str(corp_bleu))
 
+average_BLEU_data("baseline-greedy.csv")
+average_BLEU_data("reinforce-greedy.csv")
 average_BLEU_data("baseline-beam.csv")
 average_BLEU_data("reinforce-beam.csv")
